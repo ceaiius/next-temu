faca82f86eed7cbf5ab2d89b5511ff52050732fa
Nika Mamaladze <n_mamaladze@cu.edu.ge>
2025-04-25 23:01:17 +0400

    Update route.ts

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -52,13 +52,14 @@ export async function POST(req: Request) {
         switch(event.type) {
             case 'checkout.session.completed': {
                 const session = event.data.object as Stripe.Checkout.Session;
-
+                
                 const cartId = session.metadata?.cartId;
                 const userId = session.metadata?.userId;
 
                 if(!cartId) {
                     throw new Error("No cart ID in session metadata");
                 }
+                
 
                 const cart = await prisma.cart.findUnique({
                     where: {
@@ -84,16 +85,7 @@ export async function POST(req: Request) {
                     stripeCheckoutSessionId: session.id,
                     stripePaymentIntentId: session.payment_intent as string,
                     totalPrice: Number(session.amount_total) / 100,
-                    shippingAddress: {
-                        _type: 'shippingAddress',
-                        name: session.shipping_details?.name,
-                        line1: session.shipping_details?.address?.line1,
-                        line2: session.shipping_details?.address?.line2,
-                        city: session.shipping_details?.address?.city,
-                        state: session.shipping_details?.address?.state,
-                        postalCode: session.shipping_details?.address?.postal_code,
-                        country: session.shipping_details?.address?.country,
-                    },
+                    
                     orderItems: cart.items.map((item) => ({
                         _type: 'orderItem',
                         _key: item.id,
