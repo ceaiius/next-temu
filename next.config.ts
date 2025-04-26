dd6a8cacfa2d34248dd45cba6f49d86653171b51
Nika Mamaladze <n_mamaladze@cu.edu.ge>
2025-04-26 11:24:09 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
  images: {
     domains: ['cdn.sanity.io']
- }
+ },
+ typescript: {
+   ignoreBuildErrors: true,
+ },
 };
 
 export default nextConfig;
